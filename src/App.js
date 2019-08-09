81a94f8843f1e4808e84d6ebbb7c794ca54904b7
m-qm <mireiaquerol@gmail.com>
2019-08-09 20:19:05 +0200

    search bar functionality working

--- src/App.js
+++ src/App.js
@@ -1,86 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import RecipeCard from './components/RecipeCard';
 import './App.css';
-import SearchParams from './components/SearchParams';
+import SearchInput from './components/SearchInput';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      meal: {},
-      query: '',
-      isLoading: false,
-      error: null
-    }
-  }
-
-  getMeals(query) {
-    this.setState({
-      isLoading: true 
-    })
-    if(this.state.query) {
-      const URL = `https://badi-recipes.now.sh/api?i=${this.state.query}&p=1`;
-      axios.get(URL)
-        .then(res => {
-          const meal = res.data.results;
-          if(typeof meal === 'object'){
-            this.setState({ meal, isLoading: false });
-          }
-        })
-        .catch(error => this.setState({
-          error,
-          isLoading: false
-        }));
-    }
-    const DEFAULT_QUERY = 'onions,garlic'
-    const URL = `https://badi-recipes.now.sh/api?i=${DEFAULT_QUERY}&p=1`;
-    axios.get(URL)
-      .then(res => {
-        const meal = res.data.results;
-        if(typeof meal === 'object'){
-          this.setState({ meal, isLoading: false });
-        }
-      })
-      .catch(error => this.setState({
-        error,
-        isLoading: false
-      }));
-  }
-
-componentDidMount() {
-  this.getMeals()
-}
-
-onInputChange(query) {
-  this.setState({
-    query: query
-  })
-  this.getMeals(this.state.query)
-}
 
 render() {
 
-  var data = this.state.meal;
-  const {error, isLoading} = this.state;
-
-  if (!data) {
-  return <p>No data yet ...</p>;
-  }
-
-  if (error) {
-    return <p>{error.message}</p>;
-  }
-
-  if (isLoading) {
-    return <p>Loading ...</p>;
-  }
     return (
       <div className="App">
-      <SearchParams onInputChange={query => this.onInputChange(query)}/>
-        {data.length > 0 && <RecipeCard meals={data} />}
-      
+        <SearchInput />      
       </div>
     );
   }
---
+++ src/components/SearchInput.js
@@ -0,0 +1,114 @@
+import React, { Component }  from "react";
+import axios from 'axios';
+import RecipeCard from './RecipeCard'
+import './SearchInput.css'
+
+class SearchInput extends Component {
+
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			query: '',
+			results: {},
+			loading: false,
+			message: '',
+		};
+
+		this.cancel = '';
+	}
+
+  fetchSearchResults = ( query ) =>{
+    const searchUrl = `https://badi-recipes.now.sh/api?i=${this.state.query}`;
+    if(this.cancel) {
+      this.cancel.cancel()
+    }
+    this.cancel = axios.CancelToken.source();
+    axios.get( searchUrl, {
+          cancelToken: this.cancel.token
+        } )
+          .then( res => {
+            console.log(res.data)
+            const resultNotFoundMsg = ! res.data.results.length
+                        ? 'There are no search results. Please type more than 3 characters'
+                        : '';
+            this.setState( {
+              results: res.data.results,
+              message: resultNotFoundMsg,
+              loading: false
+            } )
+          } )
+          .catch( error => {
+            if ( axios.isCancel(error) || error ) {
+              this.setState({
+                loading: false,
+              })
+            }
+          } )
+  }
+
+
+  handleOnInputChange = ( event ) => {
+		const query = event.target.value;
+		if ( ! query ) {
+			this.setState( { query, results: {}, message: '' } );
+		} else {
+			this.setState( { query, loading: true, message: '' }, () => {
+				this.fetchSearchResults( query );
+			} );
+		}
+	};
+
+	/**
+	 * Fetch results according to the prev or next page requests.
+	 *
+	 * @param {String} type 'prev' or 'next'
+	 */
+
+	renderSearchResults = () => {
+		const { results } = this.state;
+
+    console.log(results)
+
+		if ( Object.keys( results ).length && results.length ) {
+			return (
+        <RecipeCard meals={results}/>
+			)
+		}
+	};
+
+	render() {
+		const { query,message, error } = this.state;
+		
+		if(error) {
+			return <p>{error.message}</p>
+		}
+
+		return (
+			
+			<div className="container">
+			<h2 className="heading">Recipe App - Badi Test</h2>
+			<label className="search-label" htmlFor="search-input">
+				<i className="fa fa-search search-icon" aria-hidden="true"/>
+
+				<input
+					type="text"
+					name="query"
+					value={ query }
+					id="search-input"
+					placeholder="Search..."
+					onChange={this.handleOnInputChange}
+				/>
+			</label>
+
+			{/*	Error Message*/}
+				{message && <p className="message">{ message }</p>}
+
+			{ this.renderSearchResults() }
+
+			</div>
+		)
+	}
+}
+
+export default SearchInput;
